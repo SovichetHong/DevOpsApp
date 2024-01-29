2b36597e6d498550b9a82d31e7675332606f02d5
Sovichet Hong <59636311+SovichetHong@users.noreply.github.com>
2024-01-29 11:27:55 +0700

    Push Code

---
+++ src/main/java/com/devops/devopsapp/DevOpsAppApplication.java
@@ -0,0 +1,13 @@
+package com.devops.devopsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devops/devopsapp/DevOpsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devops.devopsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
